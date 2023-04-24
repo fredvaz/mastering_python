fc2c7e6725d9b589200d45451623edd23983ed6a
fredvaz <fredvaz8@gmail.com>
2023-04-24 14:35:09 +0100

    debugging

---
+++ debug/example_app.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+
+# Lecture 184: How to Debug Code
+# debugging
+
+# linting: 
+# allows us to detect as we code some issues with our code, formating, read errors
+# 
+# IDE pycharm has a linting by default
+# On VS Code we must install a extension
+
+# pdb
+import pdb
+
+def add(num1, num2):
+    # print(num1, num2) # hard debugging
+    # instead we can use the pdb: gives a interactive python debugger
+    # that we can now type commands e.g., type num1, num2, step, help, exit
+    pdb.set_trace() 
+    t = 4 * 5
+    return num1 + num2
+    
+
+add(4, 'asdfr')
+
+
+# def myFunction():
+#     print("Hello from my function")
+
+# if __name__ == "__main__":
+#     myFunction()
